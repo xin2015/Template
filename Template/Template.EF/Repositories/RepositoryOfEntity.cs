2f88fbff79465493d00605717a604ad333847ba2
LiuHaixin <liuhaixin2016@outlook.com>
2016-11-06 19:54:34 +0800

    Repository

---
+++ Template/Template.EF/Repositories/RepositoryOfEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Template.Core.Entities;
+
+namespace Template.EF.Repositories
+{
+    public class Repository<TEntity> : Repository<TEntity, int> where TEntity : Entity
+    {
+    }
+}
